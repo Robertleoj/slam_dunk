ea2801c6e6a92514dcb065ba523487cad930ec73
Robertleoj <robert.leo.jonsson@gmail.com>
2025-04-08 17:00:48 +0200

    window prototype

--- include/slamd/window.hpp
+++ include/slamd/window.hpp
@@ -1,7 +1,10 @@
 #pragma once
 #include <cstddef>
+#include <memory>
 #include <slamd/glfw.hpp>
+#include <slamd/scene.hpp>
 #include <thread>
+#include <vector>
 
 namespace slamd {
 class Window {
@@ -11,9 +14,13 @@ class Window {
    private:
     void render_job(size_t height, size_t width);
 
+    ~Window();
+
    private:
     std::thread render_thread;
     GLFWwindow* window = nullptr;
+    std::map<std::string, std::shared_ptr<Scene>> scenes;
+    bool should_stop = false;
 };
 
 }  // namespace slamd
--- src/window.cpp
+++ src/window.cpp
@@ -1,3 +1,7 @@
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <spdlog/spdlog.h>
 #include <slamd/window.hpp>
 
 namespace slamd {
@@ -9,11 +13,64 @@ Window::Window(
     this->render_thread = std::thread(&Window::render_job, this, height, width);
 }
 
+void framebuffer_size_callback(
+    GLFWwindow* window,
+    int width,
+    int height
+) {
+    spdlog::debug("Window resized");
+    gl::glViewport(0, 0, width, height);
+}
+
 void Window::render_job(
     size_t height,
     size_t width
 ) {
     this->window = slamd::glutils::make_window("Slam Dunk", width, height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigWindowsMoveFromTitleBarOnly = true;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    while (!glfwWindowShouldClose(window) && !should_stop) {
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGuiID main_dockspace_id = ImGui::DockSpaceOverViewport();
+
+        for (auto& [scene_name, scene] : this->scenes) {
+            ImGui::SetNextWindowDockID(main_dockspace_id, ImGuiCond_Once);
+            ImGui::Begin(scene_name.c_str());
+            scene->render_to_imgui();
+            ImGui::End();
+        }
+
+        ImGui::Render();
+
+        gl::glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        gl::glClear(gl::GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
+
+Window::~Window() {
+    should_stop = true;
+    if (this->render_thread.joinable()) {
+        this->render_thread.join();
+    }
 }
 
 }  // namespace slamd
