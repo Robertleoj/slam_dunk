f643a84b003662e227968f7b814c1992fbd2df28
Robertleoj <robert.leo.jonsson@gmail.com>
2025-04-09 12:25:33 +0200

    add example

---
+++ examples/moving_box/main.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <glm/glm.hpp>
+#include <slamd/angle.hpp>
+#include <slamd/slamd.hpp>
+#include <slamd/transforms.hpp>
+#include <thread>
+
+int main() {
+    slamd::Window window(1000, 1000);
+
+    auto scene = slamd::scene();
+
+    scene->set_object("/rot/trans/box", slamd::geometry::box());
+
+    scene->set_transform(
+        "/rot/trans", slamd::transl(glm::vec3(10.0, 0.0, 0.0))
+    );
+
+    glm::mat4 current_rot = glm::mat4(1.0);
+
+    window.add_scene("scene", scene);
+
+    while (true) {
+        // rotate by 1 degree every 10ms
+        current_rot = slamd::rz(slamd::Angle::deg(1.0)) * current_rot;
+
+        scene->set_transform("/rot", current_rot);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
