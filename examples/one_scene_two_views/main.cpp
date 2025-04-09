3f9fc5a975855d61e742d042cee260ee9156cf21
Robertleoj <robert.leo.jonsson@gmail.com>
2025-04-09 12:14:23 +0200

    update example

--- examples/one_scene_two_views/main.cpp
+++ examples/one_scene_two_views/main.cpp
@@ -5,10 +5,13 @@ int main() {
 
     auto scene = slamd::scene();
 
+    // add the scene twice - this will give
+    // two views of the same scene
     window.add_scene("scene view 1", scene);
     window.add_scene("scene view 2", scene);
 
-    scene->set_object("/box", slamd::geometry::triad());
+    // example object - triad at origin
+    scene->set_object("/origin", slamd::geometry::triad());
 
     window.wait_for_close();
 }
