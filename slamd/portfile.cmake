2ea6ec8e34b93f5840690b6c6da431d462caa88c
Robertleoj <robert.leo.jonsson@gmail.com>
2025-04-09 15:11:31 +0200

    wip

--- slamd/portfile.cmake
+++ slamd/portfile.cmake
@@ -1,21 +1,16 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH_RAW
+    OUT_SOURCE_PATH REPO_ROOT
     REPO Robertleoj/slam_dunk
     REF bundle_with_vcpkg
-    SHA512 9b4867cb293b76144f912f4539bc665f9ace4105beccd98f3c1fb37799c8e285e05ccc54f2fb7034e2788ed69c3c25e20ab9685f58426a82482d82070b16236a
+    SHA512 89435f685d8092c2dd54ddd1223da20858330dc18861026afa0c72f01995bc44a65e1701208ecbeba3f2bbf6b96dc3229fc4e3a93534a1c81e661342120d7105
     HEAD_REF bundle_with_vcpkg
 )
 
-
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH_RAW}/slamd"
+    SOURCE_PATH "${REPO_ROOT}/slamd"
 )
-
-# vcpkg_cmake_configure(
-#     SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}"  # this is your real source folder
-# )
 
 vcpkg_cmake_install()
 
@@ -23,7 +18,7 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME "slamd")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH_RAW}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${REPO_ROOT}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 # file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
