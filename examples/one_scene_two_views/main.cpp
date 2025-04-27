dcba22aa812498b059973b64873e2a3c8e6601b4
Robertleoj <robert.leo.jonsson@gmail.com>
2025-04-27 18:21:33 +0200

    scene works

--- examples/one_scene_two_views/main.cpp
+++ examples/one_scene_two_views/main.cpp
@@ -1,17 +1,17 @@
 #include <slamd/slamd.hpp>
 
 int main() {
-    slamd::Window window("one_scene_two_views", 1000, 1000);
+    slamd::Visualizer vis("one_scene_two_views");
 
     auto scene = slamd::scene();
 
     // add the scene twice - this will give
     // two views of the same scene
-    window.add_scene("scene view 1", scene);
-    window.add_scene("scene view 2", scene);
+    vis.add_scene("scene view 1", scene);
+    vis.add_scene("scene view 2", scene);
 
     // example object - triad at origin
     scene->set_object("/origin", slamd::geom::triad());
 
-    window.wait_for_close();
+    vis.hang_forever();
 }
